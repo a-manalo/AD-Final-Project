c933dbaff47c3bf7ad31035dff1e40ebf7a15f0c
Hazel Mito <hazelmito2029@gmail.com>
2025-07-05 20:08:52 +0800

    feat(navbar.component.php): Added navbar menu

--- components/componentGroup/navbar.component.php
+++ components/componentGroup/navbar.component.php
@@ -3,5 +3,12 @@
         <a href="/" class="logo">
             <img src="/assets/img/Website-Logo.png" alt="">
         </a>
+
+        <ul class="navbar-menu">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Products</a></li>
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Login</a></li>
+        </ul>
     </div>
 </nav>
