e779ad4a8cd6a044f2a042114fa6ce9042abb774
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 18:25:39 +0800

    feat: Added html

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
-
 include 'layouts/main.layout.php';
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Forbidden Codex</title>
+</head>
+<body>
+    
+</body>
+</html>
