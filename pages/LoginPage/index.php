4370d744f309129e6fa2a344b5bba37ed001bb08
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 22:58:35 +0800

    refractor(LoginPage): Changed img for login

--- pages/LoginPage/index.php
+++ pages/LoginPage/index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <title> Login and Registration Form in HTML & CSS | CodingLab </title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/pages/LoginPage/assets/css/login.css">
     <!-- Fontawesome CDN Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,14 +14,14 @@
     <input type="checkbox" id="flip">
     <div class="cover">
       <div class="front">
-        <img src="images/frontImg.jpg" alt="">
+        <img src="/assets/img/Background.jpg" alt="">
         <div class="text">
           <span class="text-1">Every new friend is a <br> new adventure</span>
           <span class="text-2">Let's get connected</span>
         </div>
       </div>
       <div class="back">
-        <!--<img class="backImg" src="images/backImg.jpg" alt="">-->
+        <img class="backImg" src="/assets/img/Background.jpg" alt="">
         <div class="text">
           <span class="text-1">Complete miles of journey <br> with one step</span>
           <span class="text-2">Let's get started</span>
