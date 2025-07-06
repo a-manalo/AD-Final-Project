cc2deabaeadf1c390cf32726920f72924649e8cd
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 21:49:43 +0800

    feat: Added bootstrap css

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ include 'layouts/main.layout.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Forbidden Codex</title>
     <link rel="stylesheet" href="/assets/css/hero.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
     <!--Hero Section-->
