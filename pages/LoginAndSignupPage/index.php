c6904b742d1fb0bf752b7edd6697c843b81262f2
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 16:45:07 +0800

    feat(LoginAndSignupPage): Added main.css link to index.php

--- pages/LoginAndSignupPage/index.php
+++ pages/LoginAndSignupPage/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/pages/LoginAndSignupPage/assets/css/loginAndSignup.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
     <title>LoginForm</title>
 </head>
 <body>
