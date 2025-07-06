e458b04825429cb6750bac222fd82bbd3e4d72e4
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 14:15:41 +0800

    feat(LoginAndSignupPage): Added form

---
+++ pages/LoginAndSignupPage/index.php
@@ -0,0 +1,6 @@
+<div class="login">
+    <div class="container">
+        <h1>Welcome Back!</h1>
+        <p>If you need a friend to slip your desired materials in your hands, just know The Forbidden Codex is that friend. Proceed with caution.</p>
+    </div>
+</div>
