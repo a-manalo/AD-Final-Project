1299643d34dd27f3adc5228c6e33eb23a8377db3
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 20:27:52 +0800

    feat: Added hero section

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php 
 include 'layouts/main.layout.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Forbidden Codex</title>
+</head>
+<body>
+    <!--Hero Section-->
+    <div class="hero">
+        <div class="container">
+            <img src="/assets/img/hero.png" alt="">
+        </div>
+    </div>
+
+</body>
+</html>
