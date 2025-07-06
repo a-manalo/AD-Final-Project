dd0dd23d875b80677fa94d24177f1f61c0cedea7
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 22:37:40 +0800

    feat(LoginPage-index.php): Added css link to html

--- pages/LoginPage/index.php
+++ pages/LoginPage/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login / Sign Up</title>
-    <!-- Add your CSS and meta tags here -->
+    <link rel="stylesheet" href="/pages/LoginPage/assets/css/login.css">
 </head>
 <body>
 
