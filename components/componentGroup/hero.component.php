184f98b9b016ade062cc86fe7fd5e66b10081b53
Hazel Mito <hazelmito2029@gmail.com>
2025-07-06 10:21:42 +0800

    feat(hero.component.php): Added classes and texts

---
+++ components/componentGroup/hero.component.php
@@ -0,0 +1,8 @@
+<section class="hero">
+    <div class="hero-container">
+        <div class="hero-text">
+            <h1>The Forbidden Codex</h1>
+            <p>A smart site that is filled with treasures and services from the darkest world. Explore with caution and precision.</p>
+        </div>
+    </div>
+</section>
