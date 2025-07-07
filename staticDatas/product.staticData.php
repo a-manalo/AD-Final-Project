4e79cc427b2001469566a103884b255e58ed7678
Hazel Mito <hazelmito2029@gmail.com>
2025-07-07 17:14:58 +0800

    feat(product.staticData.php): Added temporary data as dummies

---
+++ staticDatas/product.staticData.php
@@ -0,0 +1,19 @@
+<?php
+$products = [
+    [
+        'name' => 'Crimson Hoodie',
+        'price' => 1299.99,
+        'image' => '/assets/img/crimson-hoodie.webp'
+    ],
+    [
+        'name' => 'Shadow Sneakers',
+        'price' => 2199.00,
+        'image' => '/assets/img/shadow-sneakers.jpg'
+    ],
+    [
+        'name' => 'Nightfall Jacket',
+        'price' => 3299.50,
+        'image' => '/assets/img/nightfall-jacket.png'
+    ]
+];
+?>
