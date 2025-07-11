b1ec2e45e1ee7cf9438bb82500dfdf4c1c4ab9c4
RiaVenice <riatcruz.v1@gmail.com>
2025-07-11 20:41:07 +0800

    refractor(transaction.staticData.php): changed user id to match users.staticData.php

--- staticDatas/dummies/transactions.staticData.php
+++ staticDatas/dummies/transactions.staticData.php
@@ -2,19 +2,19 @@
 return [
     [
         'id' => 'c9e8bff2-a1a7-4fd6-b245-6c14834b2d11',
-        'user_id' => '3f4a9d6b-274f-4c23-98df-df56cbbb1b11', // Buyer 1
+        'user_id' => '183e26d8-e063-49ba-a55c-bd412ec43f41', // Buyer 1
         'total_amount' => 25000.00,
         'status' => 'Completed'
     ],
     [
         'id' => 'dcfb547a-cdb4-4dc1-b4f1-f33e6ed97c2e',
-        'user_id' => 'b1e5ffb3-b3f7-43d0-9fbd-9df0a4517de5', // Buyer 2
+        'user_id' => '58904366-9bb2-4040-8f4b-8ee66bf647be', // Buyer 2
         'total_amount' => 13800.00,
         'status' => 'Completed'
     ],
     [
         'id' => '1ef76b55-b9df-42c2-bebc-108e95e78a80',
-        'user_id' => '44b019f4-72f1-4a08-8898-558d12422ee2', // Buyer 3
+        'user_id' => 'fd56fbad-93f2-4994-97e9-ad9f31833a20', // Buyer 3
         'total_amount' => 31000.00,
         'status' => 'Pending'
     ]
