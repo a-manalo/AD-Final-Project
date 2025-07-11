08c9e8333684c48313d0a4c7a8a428e4b0a2b63c
RiaVenice <riatcruz.v1@gmail.com>
2025-07-11 20:48:05 +0800

    refractor(payments.staticData.php): changed id

--- staticDatas/dummies/payments.staticData.php
+++ staticDatas/dummies/payments.staticData.php
@@ -1,19 +1,19 @@
 <?php
 return [
     [
-        'transaction_id' => 'tx-ae25d23c-cc11-4f0a-b17a-000000000001', // Buyer One
+        'transaction_id' => 'c9e8bff2-a1a7-4fd6-b245-6c14834b2d11', // Buyer One
         'payment_method' => 'Credit Card',
         'amount_paid' => 25000.00,
         'payment_status' => 'Paid'
     ],
     [
-        'transaction_id' => 'tx-ae25d23c-cc11-4f0a-b17a-000000000002', // Buyer Two
+        'transaction_id' => 'dcfb547a-cdb4-4dc1-b4f1-f33e6ed97c2e', // Buyer Two
         'payment_method' => 'PayPal',
         'amount_paid' => 13800.00,
         'payment_status' => 'Paid'
     ],
     [
-        'transaction_id' => 'tx-ae25d23c-cc11-4f0a-b17a-000000000003', // Buyer Three
+        'transaction_id' => '1ef76b55-b9df-42c2-bebc-108e95e78a80', // Buyer Three
         'payment_method' => 'Crypto',
         'amount_paid' => 31000.00,
         'payment_status' => 'Unpaid'
