3b70671d819fb7a2d08cd0ecca942d5541c73ae6
RiaVenice <riatcruz.v1@gmail.com>
2025-07-11 17:42:31 +0800

    feat(users.staticData.php): added dummy datas (sellers, admins, buyers)

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,81 @@
+<?php
+return [
+
+    // Sellers
+    [
+        'username' => 'shadownymph',
+        'password' => 'nymphPass123!',
+        'email'    => 'shadow@sellers.black',
+        'role'     => 'seller',
+    ],
+    [
+        'username' => 'duskwhisper',
+        'password' => 'duskPower456!',
+        'email'    => 'dusk@sellers.black',
+        'role'     => 'seller',
+    ],
+    [
+        'username' => 'voidsight',
+        'password' => 'voidSight789!',
+        'email'    => 'void@sellers.black',
+        'role'     => 'seller',
+    ],
+    [
+        'username' => 'onmyoshade',
+        'password' => 'onmyoShade321!',
+        'email'    => 'onmyo@sellers.black',
+        'role'     => 'seller',
+    ],
+    [
+        'username' => 'gravebloom',
+        'password' => 'graveBloom654!',
+        'email'    => 'grave@sellers.black',
+        'role'     => 'seller',
+    ],
+
+    // Admins
+    [
+        'username' => 'admin.oracle',
+        'password' => 'adminPower1!',
+        'email'    => 'oracle@admin.black',
+        'role'     => 'admin',
+    ],
+    [
+        'username' => 'admin.aether',
+        'password' => 'adminPower2!',
+        'email'    => 'aether@admin.black',
+        'role'     => 'admin',
+    ],
+    [
+        'username' => 'admin.chronos',
+        'password' => 'adminPower3!',
+        'email'    => 'chronos@admin.black',
+        'role'     => 'admin',
+    ],
+    [
+        'username' => 'admin.hecate',
+        'password' => 'adminPower4!',
+        'email'    => 'hecate@admin.black',
+        'role'     => 'admin',
+    ],
+
+    // Buyers (anonymous usernames)
+    [
+        'username' => 'cobramoon94',
+        'password' => 'buyerSafe1!',
+        'email'    => 'cobra@buyers.black',
+        'role'     => 'buyer',
+    ],
+    [
+        'username' => 'emberwolf22',
+        'password' => 'buyerSafe2!',
+        'email'    => 'ember@buyers.black',
+        'role'     => 'buyer',
+    ],
+    [
+        'username' => 'obsidianfox77',
+        'password' => 'buyerSafe3!',
+        'email'    => 'obsidian@buyers.black',
+        'role'     => 'buyer',
+    ],
+];
