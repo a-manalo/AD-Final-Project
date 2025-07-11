9a39b9f9c83417ccd9e8a88bac160e02889cf498
RiaVenice <riatcruz.v1@gmail.com>
2025-07-11 18:28:20 +0800

    feat(bootstrap.php): added dummies path for staticDatas

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,6 @@ define('BASE_PATH', realpath(__DIR__));
 
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
 
 chdir(BASE_PATH);
