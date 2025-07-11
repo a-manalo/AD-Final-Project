26bfba913b4af9e328cc97f5d1e4db7b4b705673
RiaVenice <riatcruz.v1@gmail.com>
2025-07-11 18:20:22 +0800

    feat(transactions.staticData.php): added dummy data for transactions

---
+++ staticDatas/dummies/transactions.staticData.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    [
+        'id' => 'c9e8bff2-a1a7-4fd6-b245-6c14834b2d11',
+        'user_id' => '3f4a9d6b-274f-4c23-98df-df56cbbb1b11', // Buyer 1
+        'total_amount' => 25000.00,
+        'status' => 'Completed'
+    ],
+    [
+        'id' => 'dcfb547a-cdb4-4dc1-b4f1-f33e6ed97c2e',
+        'user_id' => 'b1e5ffb3-b3f7-43d0-9fbd-9df0a4517de5', // Buyer 2
+        'total_amount' => 13800.00,
+        'status' => 'Completed'
+    ],
+    [
+        'id' => '1ef76b55-b9df-42c2-bebc-108e95e78a80',
+        'user_id' => '44b019f4-72f1-4a08-8898-558d12422ee2', // Buyer 3
+        'total_amount' => 31000.00,
+        'status' => 'Pending'
+    ]
+];
