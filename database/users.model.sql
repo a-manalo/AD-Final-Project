1d57bd2c76d42e62c233d34911df785caf4874af
RiaVenice <riatcruz.v1@gmail.com>
2025-07-13 22:40:47 +0800

    refractor(users.model.sql): added default value to role

--- database/users.model.sql
+++ database/users.model.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS public."users" (
     id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     username varchar(225) NOT NULL UNIQUE,
     password varchar(225) NOT NULL,
-    email varchar(255) NOT NULL UNIQUE,
-    role varchar(50) NOT NULL, -- e.g., customer, admin
+    email varchar(255) UNIQUE, 
+    role varchar(50) NOT NULL DEFAULT 'buyer', -- other roles are seller, admin
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
