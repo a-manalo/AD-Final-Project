f8b941011171714569afd35bb4e3ce6865e04198
Hazel Mito <hazelmito2029@gmail.com>
2025-07-13 21:02:41 +0800

    feat(navbar.component.php): Added the following - Site disclaimer - Condition to show Account button if logged in and login btn if not

--- components/templates/navbar.component.php
+++ components/templates/navbar.component.php
@@ -3,22 +3,27 @@
 </div>
 
 <nav class="navbar">
-    <div class="container">
-        <a href="/" class="logo">
-            <img src="/assets/img/website_logo.png" alt="">
-        </a>
-        <div class="brand-text">
-            <a href="/" class = "website-name">The Forbidden Codex</a>
-            <p class = "company-name">Cottonee Inc.</p>
-        </div>
+  <div class="container">
+    <a href="/" class="logo">
+      <img src="/assets/img/website_logo.png" alt="">
+    </a>
+    <div class="brand-text">
+      <a href="/" class="website-name">The Forbidden Codex</a>
+      <p class="company-name">Cottonee Inc.</p>
+    </div>
 
-        <div class="navbar-menu">
-            <ul class="navbar-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/pages/ProductPage/index.php">Products</a></li>
-                <li><a href="/">Services</a></li>
-                <li><a href="/" class = "login-btn">Account</a></li>
-            </ul>
-        </div>
+    <div class="navbar-menu">
+      <ul class="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/pages/ProductPage/index.php">Products</a></li>
+        <li><a href="/">Services</a></li>
 
+        <?php if (isset($_SESSION['user']) && ($_SESSION['user']['role'] ?? '') === 'user'): ?>
+          <li><a href="/pages/account/index.php" class="login-btn">Account</a></li>
+        <?php else: ?>
+          <li><a href="/pages/login/index.php" class="login-btn">Login</a></li>
+        <?php endif; ?>
+      </ul>
+    </div>
+  </div>
 </nav>
