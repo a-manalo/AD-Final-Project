b2fea9a07df24ff1b74c9ac731d30a1aebe2af75
RiaVenice <riatcruz.v1@gmail.com>
2025-07-14 17:53:49 +0800

    refractor(users.staticData.php): removed a dummy data

--- staticDatas/dummies/users.staticData.php
+++ staticDatas/dummies/users.staticData.php
@@ -95,7 +95,5 @@ return [
         'id' => 'c42f60ab-d5b7-4783-8ed2-9f63a1287c77',
         'username' => 'voidcipher',
         'password' => 'xeno9veil',
-        'email' => null,
-        'role' => null
     ],
 ];
