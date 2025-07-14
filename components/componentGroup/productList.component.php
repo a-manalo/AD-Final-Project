3b64c802a531d5d3f5d5f6010b2eef09c1cd83ba
Hazel Mito <hazelmito2029@gmail.com>
2025-07-15 06:14:20 +0800

    refactor(productList.component.php): Updated require path

--- components/componentGroup/productList.component.php
+++ components/componentGroup/productList.component.php
@@ -1,7 +1,7 @@
-<?php require_once __DIR__ . '/../../staticDatas/product.staticData.php'?>
+<?php require_once STATICDATAS_PATH . "/product.staticData.php"?>
 
 <div class="product-grid">
     <?php foreach ($products as $product): ?>
-        <?php include __DIR__ . '/../../components/templates/productCard.component.php'; ?>
+        <?php include TEMPLATES_PATH . "/productCard.component.php";?>
     <?php endforeach; ?>
 </div>
