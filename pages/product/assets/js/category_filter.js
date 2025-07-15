89a4151a0e4857d864cb008110bd3473715268f1
Hazel Mito <hazelmito2029@gmail.com>
2025-07-15 13:41:04 +0800

    refactor(category_filter.js): Updated filter

--- pages/product/assets/js/category_filter.js
+++ pages/product/assets/js/category_filter.js
@@ -1,5 +1,5 @@
 const filterLinks = document.querySelectorAll("[data-filter]");
-const productCards = document.querySelectorAll(".product-card");
+const sections = document.querySelectorAll(".product-section");
 const title = document.getElementById("category-title");
 
 filterLinks.forEach((link) => {
@@ -7,22 +7,18 @@ filterLinks.forEach((link) => {
     e.preventDefault();
     const filter = this.getAttribute("data-filter");
 
-    // Toggle active class
     filterLinks.forEach((l) => l.classList.remove("active"));
     this.classList.add("active");
 
-    //Updates Category Title
-    //Ex: User click on artifacts title will display "Artifacts"
     title.textContent =
       filter === "all"
         ? "All Products"
-        : filter.charAt(0).toUpperCase() + filter.slice(1); // Capitalize
+        : filter.charAt(0).toUpperCase() + filter.slice(1);
 
-    //Filters Product to show products in each category
-    productCards.forEach((card) => {
-      const category = card.getAttribute("data-category");
-      card.style.display =
-        filter === "all" || filter === category ? "block" : "none";
+    sections.forEach((section) => {
+      const sectionCategory = section.getAttribute("data-category");
+      section.style.display =
+        filter === "all" || sectionCategory === filter ? "block" : "none";
     });
   });
 });
