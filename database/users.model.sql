05e8b158f638b183e77e624e709a1b6ae1fce0f0
a-manalo <agmnlo.by@gmail.com>
2025-07-16 21:58:08 +0800

    refactor(users.model.sql): added seller_request_status field to users table

--- database/users.model.sql
+++ database/users.model.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS public."users" (
     id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     username varchar(225) NOT NULL UNIQUE,
     password varchar(225) NOT NULL,
-    email varchar(255) UNIQUE, 
-    role varchar(50) NOT NULL DEFAULT 'buyer', -- other roles are seller, admin
+    email varchar(255) UNIQUE,
+    role varchar(50) NOT NULL DEFAULT 'buyer', -- other roles are seller and admin
+    seller_request_status VARCHAR(20) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
