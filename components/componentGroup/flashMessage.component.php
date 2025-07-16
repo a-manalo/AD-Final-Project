6e1fba058379049d9d7997af78e4669d0006f61a
Hazel Mito <hazelmito2029@gmail.com>
2025-07-16 09:08:26 +0800

    refactor(flashMessage.component.php): Deleted flash message component

--- components/componentGroup/flashMessage.component.php
+++
@@ -1,9 +0,0 @@
-<?php if (isset($_SESSION['success'])): ?>
-    <div class="success">
-        <?= $_SESSION['success']; unset($_SESSION['success']); ?>
-    </div>
-<?php elseif (isset($_SESSION['error'])): ?>
-    <div class="error">
-        <?= $_SESSION['error']; unset($_SESSION['error']); ?>
-    </div>
-<?php endif; ?>
