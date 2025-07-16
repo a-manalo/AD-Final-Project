9435d78f4f1194a2462128b552681e3e2a8a3ed8
Hazel Mito <hazelmito2029@gmail.com>
2025-07-16 09:02:54 +0800

    feat(foot.component.php): Added foot component

---
+++ components/templates/foot.component.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once TEMPLATES_PATH . "/footer.component.php";
+
+function footer(array $pageJs = [])
+{
+    footerComponent();
+    ?>
+    </main>
+    
+    <script src="/assets/js/footer.js"></script>
+
+    <?php
+    if (!empty($pageJs)) {
+        foreach ($pageJs as $jsFile) {
+            echo "<script src=\"{$jsFile}\"></script>\n";
+        }
+    }
+    ?>
+
+    </body>
+
+    </html>
+    <?php
+}
+?>
