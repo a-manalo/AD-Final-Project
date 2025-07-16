b52760ccef3b5b9f92769ed68c7088a9af62b16b
a-manalo <agmnlo.by@gmail.com>
2025-07-17 00:59:16 +0800

    fix(payments.staticData.php): update payment methods for consistency across transactions

--- staticDatas/dummies/payments.staticData.php
+++ staticDatas/dummies/payments.staticData.php
@@ -2,19 +2,19 @@
 return [
     [
         'transaction_id' => 'c9e8bff2-a1a7-4fd6-b245-6c14834b2d11', // Buyer One
-        'payment_method' => 'Credit Card',
+        'payment_method' => 'Bitcoin',
         'amount_paid' => 25000.00,
         'payment_status' => 'Paid'
     ],
     [
         'transaction_id' => 'dcfb547a-cdb4-4dc1-b4f1-f33e6ed97c2e', // Buyer Two
-        'payment_method' => 'PayPal',
+        'payment_method' => 'Bitcoin',
         'amount_paid' => 13800.00,
         'payment_status' => 'Paid'
     ],
     [
         'transaction_id' => '1ef76b55-b9df-42c2-bebc-108e95e78a80', // Buyer Three
-        'payment_method' => 'Crypto',
+        'payment_method' => 'Eye Contact',
         'amount_paid' => 31000.00,
         'payment_status' => 'Unpaid'
     ]
