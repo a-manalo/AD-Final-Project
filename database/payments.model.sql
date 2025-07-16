937f0bef0edb733c672378a548453cb0918bd148
a-manalo <agmnlo.by@gmail.com>
2025-07-17 00:57:15 +0800

    fix(payments.model.sql): make amount_paid column nullable

--- database/payments.model.sql
+++ database/payments.model.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS payments (
     id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     transaction_id uuid NOT NULL REFERENCES transactions (id),
     payment_method VARCHAR(100), -- 'Bitcoin' or 'In-Person'
-    amount_paid DECIMAL(10, 2) NOT NULL,
+    amount_paid DECIMAL(10, 2),
     payment_status VARCHAR(50) DEFAULT 'Unpaid', -- e.g., Paid, Failed
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
