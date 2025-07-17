e811f61cfc5100a4505e07dae4e83cd519c62242
a-manalo <agmnlo.by@gmail.com>
2025-07-17 09:30:12 +0800

    fix(router.php): corrected logic again

--- router.php
+++ router.php
@@ -1,21 +1,26 @@
 <?php
 ob_start();
+
 require __DIR__ . '/bootstrap.php';
 
 if (php_sapi_name() === 'cli-server') {
     $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $file = BASE_PATH . $urlPath;
+
     if (is_file($file)) {
         return false;
     }
 
-    if (!is_file($file)) {
+    $hasExtension = pathinfo($urlPath, PATHINFO_EXTENSION);
+    if ($hasExtension && !is_file($file)) {
         http_response_code(404);
         require ERRORS_PATH . '/_404.error.php';
         exit;
     }
+
+    require BASE_PATH . '/index.php';
+    exit;
 }
 
-ob_end_flush();
 require BASE_PATH . '/index.php';
 exit;
